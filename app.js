b1d87b64eb3d9a30774027975dbeb06ef724cf17
lukini <lucas_masciotra@hotmail.com>
2018-09-18 08:32:46 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+var express = require('express');
+const path = require('path')
+const PORT = process.env.PORT || 5000
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+require('./models/user');
+
+
+//connect to MongoDB
+mongoose.connect('mongodb://lukini:masciotra26@ds151612.mlab.com:51612/caminantesgo');
+var db = mongoose.connection;
+
+//handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  // we're connected!
+});
+
+//use sessions for tracking logins
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// serve static files from template
+app.use(express.static(__dirname + '/templateLogReg'));
+
+
+// include routes
+var routes = require('./routes/router');
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+//view engine set
+app.set('views', './views') ;
+app.set('view engine', 'pug') ;
+app.use(express.static(path.join(__dirname, './public')));
+
+
+
+
+
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+
+// listen on port 3000
+app.listen(PORT, function () {
+  console.log('Express app listening on ',PORT);
+});
