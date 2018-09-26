6d3a89e4472aa808422b9cd7891df0c90fd7ee2e
lukini <lucas_masciotra@hotmail.com>
2018-09-26 12:56:57 -0300

    fuckdapolice

---
+++ templateLogReg/js/pokearray.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function() {
+$("li a i").click(function() {
+if(this.style.color == "blue") {
+    this.style.color = "black";
+    $('#coleccion[1][1]').val('test'); 
+    return 0 ; }
+else{
+    this.style.color = "blue" ; 
+      return 1
+
+}
+
+
+
+ 
+
+
+
+
+
+})})
